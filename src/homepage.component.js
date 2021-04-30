f2e1bf7f3074d3a41bb3598f7070d67755fb711b
revanthkumaar <iamrevanthkumar@gmail.com>
2021-04-30 11:07:26 +0530

    alsdj
    ljdlaskjd

---
+++ src/homepage.component.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+const HomePage = () => (
+    <div className="homePageComp">
+    homepage 
+        <div className="main-container">
+        main container
+            <div className="image-container">
+                    <div className="content">
+                    <h1 className="title">FLOWERS</h1>
+                    <span className="description"></span>
+                    </div>
+            </div>
+            <div className="image-container">
+                    <div className="content">
+                    <h1 className="title">FLOWERS</h1>
+                    <span className="description"></span>
+                    </div>
+            </div>
+            <div className="image-container">
+                    <div className="content">
+                    <h1 className="title">FLOWERS</h1>
+                    <span className="description"></span>
+                    </div>
+            </div>
+            <div className="image-container">
+                    <div className="content">
+                    <h1 className="title">FLOWERS</h1>
+                    <span className="description"></span>
+                    </div>
+            </div>
+            <div className="image-container">
+                    <div className="content">
+                    <h1 className="title">FLOWERS</h1>
+                    <span className="description"></span>
+                    </div>
+            </div>
+            <div className="image-container">
+                    <div className="content">
+                    <h1 className="title">FLOWERS</h1>
+                    <span className="description"></span>
+                    </div>
+            </div>
+            
+        </div>
+    </div>
+)
+
+export default HomePage
+//export default <component name>
