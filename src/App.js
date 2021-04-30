94fe7aec7c93fa0368efad3bfc1aa6798bc20c07
revanthkumaar <iamrevanthkumar@gmail.com>
2021-04-30 15:54:25 +0530

    fladsjhfl
    qelahkdsfl

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
+import { Route } from 'react-router-dom'
+import SignInPage from './pages/signin-page/signin-page.component'
 
 function App() {
-
-
   return (
     <div className="mainApp">
-      <HomePage /> 
+    <Route exact path="/" component={HomePage}/> 
+    <Route exact path="/signin" component={SignInPage}/>
     </div>
   );
 }
-
-
 export default App;
+
---
+++ src/pages/signin-page/signin-page.component.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './signin-page.styles.scss'
+
+const SignInPage = () => (
+   <div>
+       Signin Page
+   </div>
+)
+
+export default SignInPage
